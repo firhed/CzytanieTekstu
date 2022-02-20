d84ece3e98748612109f5115879750c2ad71477b
admin <admin@ADMIN-KOMPUTER>
2022-02-20 23:13:14 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int obieg = 1;
+            while (obieg <= 16)
+            {
+                string filePath = @"Words.txt";
+                var lines = File.ReadAllLines(filePath);
+                var r = new Random();
+                var randomLineNumber = r.Next(0, lines.Length - 1);
+                var line = lines[randomLineNumber];
+                string used = null;
+
+                int podwojenie = 0;
+                if(line != used)
+                {
+                    while (podwojenie < 2)
+                    {
+                        podwojenie++;
+                        Console.WriteLine(obieg + ". " + line);
+                    }
+                    line = used;
+                    obieg++;
+                }
+                
+            }
+        
+            Console.ReadKey();
+        }
+    }
+}
